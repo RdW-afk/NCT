0ed3c42541658eb98777cc3771aa25ca7644b811
Roman Kiryanov <rkir@google.com>
2022-06-10 19:12:05 +0530

    sdm660-common: Replace audio@2.0-service with audio.service
    Legacy service name, use android.hardware.audio.service instead
    
    Bug: 153579444
    Test: presubmit
    Signed-off-by: Roman Kiryanov <rkir@google.com>
    Merged-In: I4c05accb86dd0c9cd1b3fef3d84e0d54b2afc6a1
    Change-Id: I6b55b4b1db2577f4176c99dca7021cfe78e13307

--- sdm660.mk
+++ sdm660.mk
@@ -22,7 +22,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.audio@6.0-impl \
     android.hardware.audio.effect@6.0-impl \
-    android.hardware.audio.effect@2.0-service
+    android.hardware.audio.effect@2.0-service \
+    android.hardware.audio.service
 
 PRODUCT_PACKAGES += \
     audio.a2dp.default \
